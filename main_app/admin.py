5aadd4c8adbc9bdaa970789c52a57b6d08f66644
Jason Nguyen <j1.nguyen@yahoo.com>
2024-04-29 11:12:15 -0700

    can login/logout

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Workout, Exercise, ExerciseType
 
 # Register your models here.
+admin.site.register([
+    Workout,
+    Exercise,
+    ExerciseType
+])
